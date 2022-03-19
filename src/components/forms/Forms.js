961025fce47880dcddeee61e5c2231ce17e735cb
david_bille <David.Bille@precisionvh.com>
2022-03-19 17:32:20 -0500

    styling forms

--- src/components/forms/Forms.js
+++ src/components/forms/Forms.js
@@ -8,71 +8,71 @@ export const MedForm = ({ handleClick, handleInputChange, handleCheckboxChange, 
   }
 
   return (
-    <form>
-      <input type='text' name='name' placeholder='Medication Name' value={med.name} onChange={(e) => handleInputChange(e)} /><br/>
-      <input type='text' name='dose' placeholder='Dose' value={med.dose} onChange={(e) => handleInputChange(e)} /><br />
-      <input type='text' name='purpose' placeholder='Purpose' onChange={(e) => handleInputChange(e)} /><br />  
-      <input type='text' name='prescriber' placeholder='Prescriber' onChange={(e) => handleInputChange(e)} /><br />
+    <Styles.Form>
+      <Styles.Input type='text' name='name' placeholder='Medication Name' value={med.name} onChange={(e) => handleInputChange(e)} /><br/>
+      <Styles.Input type='text' name='dose' placeholder='Dose' value={med.dose} onChange={(e) => handleInputChange(e)} /><br />
+      <Styles.Input type='text' name='purpose' placeholder='Purpose' onChange={(e) => handleInputChange(e)} /><br />  
+      <Styles.Input type='text' name='prescriber' placeholder='Prescriber' onChange={(e) => handleInputChange(e)} /><br />
       <label htmlFor='frequency'>Frequency:</label><br />
-      <input type='checkbox' name='date-sunday' value='0' checked={isActiveBox(med.frequency, '0')} onChange={(e) => handleCheckboxChange( 'frequency', e)} />
+      <Styles.Input type='checkbox' name='date-sunday' value='0' checked={isActiveBox(med.frequency, '0')} onChange={(e) => handleCheckboxChange( 'frequency', e)} />
       <label htmlFor='date-sunday'>Sun</label>
-      <input type='checkbox' name='date-monday' value='1' checked={isActiveBox(med.frequency, '1')} onChange={(e) => handleCheckboxChange( 'frequency', e)} />
+      <Styles.Input type='checkbox' name='date-monday' value='1' checked={isActiveBox(med.frequency, '1')} onChange={(e) => handleCheckboxChange( 'frequency', e)} />
       <label htmlFor='date-monday'>Mon</label>
-      <input type='checkbox' name='date-tuesday' value='2' checked={isActiveBox(med.frequency, '2')} onChange={(e) => handleCheckboxChange( 'frequency', e)} />
+      <Styles.Input type='checkbox' name='date-tuesday' value='2' checked={isActiveBox(med.frequency, '2')} onChange={(e) => handleCheckboxChange( 'frequency', e)} />
       <label htmlFor='date-tuesday'>Tues</label>
-      <input type='checkbox' name='date-wednesday' value='3' checked={isActiveBox(med.frequency, '3')} onChange={(e) => handleCheckboxChange( 'frequency', e)} />
+      <Styles.Input type='checkbox' name='date-wednesday' value='3' checked={isActiveBox(med.frequency, '3')} onChange={(e) => handleCheckboxChange( 'frequency', e)} />
       <label htmlFor='date-wednesday'>Weds</label><br />
-      <input type='checkbox' name='date-thursday' value='4' checked={isActiveBox(med.frequency, '4')} onChange={(e) => handleCheckboxChange( 'frequency', e)} />
+      <Styles.Input type='checkbox' name='date-thursday' value='4' checked={isActiveBox(med.frequency, '4')} onChange={(e) => handleCheckboxChange( 'frequency', e)} />
       <label htmlFor='date-thursday'>Thurs</label>
-      <input type='checkbox' name='date-friday' value='5' checked={isActiveBox(med.frequency, '5')} onChange={(e) => handleCheckboxChange( 'frequency', e)} />
+      <Styles.Input type='checkbox' name='date-friday' value='5' checked={isActiveBox(med.frequency, '5')} onChange={(e) => handleCheckboxChange( 'frequency', e)} />
       <label htmlFor='date-friday'>Fri</label>
-      <input type='checkbox' name='date-saturday' value='6' checked={isActiveBox(med.frequency, '6')} onChange={(e) => handleCheckboxChange( 'frequency', e)} />
+      <Styles.Input type='checkbox' name='date-saturday' value='6' checked={isActiveBox(med.frequency, '6')} onChange={(e) => handleCheckboxChange( 'frequency', e)} />
       <label htmlFor='date-saturday'>Sat</label><br />
       <label htmlFor='daysOfWeek'>Days:</label><br />
-      <input type='checkbox' name='time-am' value='AM' checked={isActiveBox(med.time, 'AM')} onChange={(e) => handleCheckboxChange( 'time', e)}/>
+      <Styles.Input type='checkbox' name='time-am' value='AM' checked={isActiveBox(med.time, 'AM')} onChange={(e) => handleCheckboxChange( 'time', e)}/>
       <label htmlFor='time-am'>AM</label>
-      <input type='checkbox' name='time-pm' value='PM'checked={isActiveBox(med.time, 'PM')} onChange={(e) => handleCheckboxChange('time', e)}/>
+      <Styles.Input type='checkbox' name='time-pm' value='PM'checked={isActiveBox(med.time, 'PM')} onChange={(e) => handleCheckboxChange('time', e)}/>
       <label htmlFor='time-pm'>PM</label><br />  
-      <button id='save-med-btn' className='form-primary-btn' onClick={(e) => handleClick(e)}>Save</button> 
-      <button id='close-med-form-btn' className='form-secondary-btn' onClick={(e) => handleClick(e)}>Cancel</button>
-    </form>
+      <Styles.Button id='save-med-btn' className='form-primary-btn' isPrimary onClick={(e) => handleClick(e)}>Save</Styles.Button> 
+      <Styles.Button id='close-med-form-btn' className='form-secondary-btn' onClick={(e) => handleClick(e)}>Cancel</Styles.Button>
+    </Styles.Form>
   )
 }
 
 export const DoctorForm = ({ handleClick, handleInputChange, doctor }) => {
   console.log(doctor)
   return (
-    <form>
-      <input type="text" name='firstName' placeholder='First Name' value={doctor.firstName} onChange={(e) => handleInputChange(e)} /><br />
-      <input type="text" name='lastName' placeholder='Last Name' value={doctor.lastName} onChange={(e) => handleInputChange(e)} /><br />
-      <input type="text" name='credentials' placeholder='Credentials' value={doctor.credentials} onChange={(e) => handleInputChange(e)} /><br />
-      <input type="text" name='specialty' placeholder='Specialty' value={doctor.specialty} onChange={(e) => handleInputChange(e)} /><br />
-      <input type="text" name='phone' placeholder='Phone Number' value={doctor.phone} onChange={(e) => handleInputChange(e)} /><br />
-      <button id='save-doctor-btn' className='form-primary-btn' onClick={(e) => handleClick(e)}>Save</button>
-      <button id='close-doctor-form-btn' className='form-secondary-btn' onClick={(e) => handleClick(e)}>Cancel</button>
-    </form>
+    <Styles.Form>
+      <Styles.Input type="text" name='firstName' placeholder='First Name' value={doctor.firstName} onChange={(e) => handleInputChange(e)} />
+      <Styles.Input type="text" name='lastName' placeholder='Last Name' value={doctor.lastName} onChange={(e) => handleInputChange(e)} />
+      <Styles.Input type="text" name='credentials' placeholder='Credentials' value={doctor.credentials} onChange={(e) => handleInputChange(e)} />
+      <Styles.Input type="text" name='specialty' placeholder='Specialty' value={doctor.specialty} onChange={(e) => handleInputChange(e)} />
+      <Styles.Input type="text" name='phone' placeholder='Phone Number' value={doctor.phone} onChange={(e) => handleInputChange(e)} />
+      <Styles.Button id='save-doctor-btn' className='form-primary-btn' isPrimary onClick={(e) => handleClick(e)}>Save</Styles.Button>
+      <Styles.Button id='close-doctor-form-btn' className='form-secondary-btn' onClick={(e) => handleClick(e)}>Cancel</Styles.Button>
+    </Styles.Form>
   )
 }
 
 export const LoginForm = ({ handleSubmit, handleChange, user }) => {
   return (
-    <form onSubmit={(e) => handleSubmit(e)}>
-      <input type='text' name='email' placeholder='Email' value={user.email} onChange={(e) => handleChange(e)} /><br />
-      <input type='password' name='password' placeholder='Password' value={user.password} onChange={(e) => handleChange(e)} /><br />
-      <button id='user-login' className='form-primary-btn'>Log In</button>
-    </form>
+    <Styles.Form onSubmit={(e) => handleSubmit(e)}>
+      <Styles.Input type='text' name='email' placeholder='Email' value={user.email} onChange={(e) => handleChange(e)} /><br />
+      <Styles.Input type='password' name='password' placeholder='Password' value={user.password} onChange={(e) => handleChange(e)} /><br />
+      <Styles.Button id='user-login' className='form-primary-btn' isPrimary solo>Log In</Styles.Button>
+    </Styles.Form>
   )
 }
 
 export const SignupForm = ({ handleSubmit, handleChange, user }) => {
   return (
-    <form onSubmit={(e) => handleSubmit(e)}>
-      <input type='text' name='userFirst' placeholder='First Name' onChange={(e) => handleChange(e)} /><br />
-      <input type='text' name='userLast' placeholder='Last Name' onChange={(e) => handleChange(e)} /><br />
-      <input type='text' name='email' placeholder='Email' onChange={(e) => handleChange(e)} /><br />
-      <input type='password' name='password' placeholder='Username' onChange={(e) => handleChange(e)} /><br />
-      <input type='password' name='passwordConfirmation' placeholder='Username' onChange={(e) => handleChange(e)} /><br />
-      <button id='user-signup' className='form-primary-btn'>Sign Up</button>
-    </form>
+    <Styles.Form onSubmit={(e) => handleSubmit(e)}>
+      <Styles.Input type='text' name='userFirst' placeholder='First Name' onChange={(e) => handleChange(e)} /><br />
+      <Styles.Input type='text' name='userLast' placeholder='Last Name' onChange={(e) => handleChange(e)} /><br />
+      <Styles.Input type='text' name='email' placeholder='Email' onChange={(e) => handleChange(e)} /><br />
+      <Styles.Input type='password' name='password' placeholder='Username' onChange={(e) => handleChange(e)} /><br />
+      <Styles.Input type='password' name='passwordConfirmation' placeholder='Username' onChange={(e) => handleChange(e)} /><br />
+      <Styles.Button id='user-signup' className='form-primary-btn' isPrimary solo>Sign Up</Styles.Button>
+    </Styles.Form>
   )
 }
--- src/components/forms/styles.js
+++ src/components/forms/styles.js
@@ -6,6 +6,22 @@ const Styles = {
     background-color: #c4d7f2;
     border: none;
     border-bottom: 2px solid #95a3b3;
+    margin-top: 10px;
+  `,
+  Button: styled.button.attrs(props => ({
+    color: props.isPrimary ? '#82D173' : '#c4d7f2',
+    width: props.solo ? '100%' : '40%',
+    weight: props.isPrimary ? 700 : 'normal'
+  }))`
+  background-color: ${props => props.color};
+  width: ${props => props.width};
+  font-weight: ${props => props.weight};
+  border: 2px solid #26384C;
+  margin: 10px 5px;
+  border-radius: 10px;
+  `,
+  Form: styled.form`
+  text-align: center;
   `
 }
 
